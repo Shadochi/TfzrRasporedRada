ee636ffb21e93bb22f43ebb99459f80999148ec9
Nikola Bobinac <nikolabobinac@gmail.com>
2020-09-14 12:56:36 +0200

    Added SQL DB Utils library

---
+++ SQLDBUtils/SQLDBUtils/clsSqlKonekcija.cs
@@ -0,0 +1,109 @@
+﻿using System.Data.SqlClient;
+
+namespace SqlDBUtils
+{
+    public class clsSqlKonekcija
+    {
+        /* ODGOVORNOST: Konekcija na celinu baze podataka, SQL server tipa  */
+
+        #region Atributi
+        private SqlConnection pKonekcija;
+        //
+        private string pPutanjaSQLBaze;
+        private string pNazivBaze;
+        private string pNazivSQL_DBMSinstance;
+        private string pStringKonekcije;
+
+        #endregion
+
+        #region Konstruktor
+        public clsSqlKonekcija(string nazivSQL_DBMSInstance, string putanjaSqlBaze, string NazivBaze)
+        {
+            // preuzimanje vrednosti u privatne atribute
+            pPutanjaSQLBaze = putanjaSqlBaze;
+            pNazivBaze = NazivBaze;
+            pNazivSQL_DBMSinstance = nazivSQL_DBMSInstance;
+            // pripremanje stringa konekcije, dodato 22.3.2020.
+            pStringKonekcije = "";
+            pStringKonekcije = FormirajStringKonekcije();
+        }
+
+        // preklapajuci konstruktor, kada dobijemo spolja string konekcije
+        public clsSqlKonekcija(string noviStringKonekcije)
+        {
+            pStringKonekcije = noviStringKonekcije; // u ovom slucaju se ne poziva metoda FormirajStringKonekcije
+        }
+
+        // preklapajuci konstruktor, bez stringa konekcije
+        public clsSqlKonekcija()
+        {
+
+        }
+
+        #endregion
+
+        #region Privatne metode
+        private string FormirajStringKonekcije() // promenjen naziv
+        {
+            string mStringKonekcije = "";
+            if (pPutanjaSQLBaze.Length.Equals(0) || pPutanjaSQLBaze == null)
+            {
+                mStringKonekcije = "Data Source=" + pNazivSQL_DBMSinstance + " ;Initial Catalog=" + pNazivBaze + ";Integrated Security=True";
+            }
+            else
+            {
+                mStringKonekcije = "Data Source=.\\" + pNazivSQL_DBMSinstance + ";AttachDbFilename=" + pPutanjaSQLBaze + "\\" + pNazivBaze + ";Integrated Security=True;Connect Timeout=30;User Instance=True";
+            }
+            return mStringKonekcije;
+        }
+        #endregion
+
+        #region Javne metode
+
+        public string StringKonekcije
+        {
+            get { return pStringKonekcije; }
+            set { pStringKonekcije = value; }
+        }
+
+
+        public bool OtvoriKonekciju()
+        {
+            bool uspeh;
+            pKonekcija = new SqlConnection();
+            pKonekcija.ConnectionString = pStringKonekcije; // 22.3.2020. s obzirom da se na konstruktoru formira i smesta string konekcije u pStringKonekcije, onda se ovde samo otvara
+            // ovo je bolje resenje nego da se ovde pozove i kreiranje stringa konekcije, s obzirom na SOLID princip Single Responsibility i a sto jezgrovitije radimo posao koji je dat (da se ne radi ono sto nije u nazivu)
+
+            try
+            {
+                pKonekcija.Open();
+                uspeh = true;
+            }
+            catch
+            {
+                uspeh = false;
+            }
+            return uspeh;
+        }
+
+        public SqlConnection DajKonekciju()
+        {
+            return pKonekcija;
+        }
+
+        public string DajStringKonekcije() // dodato 22.3.2020.
+        {
+            return pStringKonekcije;
+        }
+
+        public void ZatvoriKonekciju()
+        {
+            pPutanjaSQLBaze = "";
+            pKonekcija.Close();
+            pKonekcija.Dispose();
+        }
+
+        #endregion
+
+    }
+}
---
+++ SQLDBUtils/SQLDBUtils/clsSqlTabela.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+//
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace SqlDBUtils
+{
+    public class clsSqlTabela
+    {
+        #region Atributi
+
+        private string pNazivTabele;
+        private clsSqlKonekcija pKonekcija;
+        private SqlDataAdapter pAdapter;
+        private System.Data.DataSet pDataSet;
+
+        #endregion
+
+        #region Konstruktor
+
+        public clsSqlTabela(clsSqlKonekcija Konekcija, string NazivTabele)
+        {
+            pKonekcija = Konekcija;
+            pNazivTabele = NazivTabele;
+        }
+
+        #endregion
+
+        #region Privatne metode
+
+        private void KreirajAdapter(string SelectUpit, string InsertUpit, string DeleteUpit, string UpdateUpit)
+        {
+            SqlCommand mSelectKomanda, mInsertKomanda, mDeleteKomanda, mUpdateKomanda;
+
+            mSelectKomanda = new SqlCommand();
+            mSelectKomanda.CommandText = SelectUpit;
+            mSelectKomanda.Connection = pKonekcija.DajKonekciju();
+
+            mInsertKomanda = new SqlCommand();
+            mInsertKomanda.CommandText = InsertUpit;
+            mInsertKomanda.Connection = pKonekcija.DajKonekciju();
+
+            mDeleteKomanda = new SqlCommand();
+            mDeleteKomanda.CommandText = DeleteUpit;
+            mDeleteKomanda.Connection = pKonekcija.DajKonekciju();
+
+            mUpdateKomanda = new SqlCommand();
+            mUpdateKomanda.CommandText = UpdateUpit;
+            mUpdateKomanda.Connection = pKonekcija.DajKonekciju();
+
+            pAdapter = new SqlDataAdapter();
+            pAdapter.SelectCommand = mSelectKomanda;
+            pAdapter.InsertCommand = mInsertKomanda;
+            pAdapter.UpdateCommand = mUpdateKomanda;
+            pAdapter.DeleteCommand = mDeleteKomanda;
+        }
+
+        private void KreirajDataset()
+        {
+            pDataSet = new System.Data.DataSet();
+            pAdapter.Fill(pDataSet, pNazivTabele);
+
+        }
+
+        private System.Data.DataTable KreirajDataTable(System.Data.DataSet noviDataSet)
+        {
+            return noviDataSet.Tables[0];
+
+        }
+
+        private void ZatvoriAdapterDataset()
+        {
+            pAdapter.Dispose();
+            pDataSet.Dispose();
+        }
+
+        #endregion
+
+        #region Javne metode
+
+        public System.Data.DataSet DajPodatke(string SelectUpit)
+        // izdvaja podatke u odnosu na dat selectupit
+        {
+            KreirajAdapter(SelectUpit, "", "", "");
+            KreirajDataset();
+            return pDataSet;
+        }
+
+        public int DajBrojSlogova()
+        {
+            int BrojSlogova = pDataSet.Tables[0].Rows.Count;
+            return BrojSlogova;
+        }
+
+        public bool IzvrsiAzuriranje(string Upit)
+        // izvrzava azuriranje unos/brisanje/izmena u odnosu na dati i upit
+        {
+
+            //
+            bool uspeh = false;
+            SqlConnection mKonekcija;
+            SqlCommand Komanda;
+            SqlTransaction mTransakcija = null;
+            try
+            {
+                mKonekcija = pKonekcija.DajKonekciju();
+                // aktivan kod  
+
+                // povezivanje
+                Komanda = new SqlCommand();
+                Komanda.Connection = mKonekcija;
+                Komanda = mKonekcija.CreateCommand();
+                // pokretanje
+                // NE TREBA OPEN JER DOBIJAMO OTVORENU KONEKCIJU KROZ KONSTRUKTOR
+                // mKonekcija.Open();
+                mTransakcija = mKonekcija.BeginTransaction();
+                Komanda.Transaction = mTransakcija;
+                Komanda.CommandText = Upit;
+                Komanda.ExecuteNonQuery();
+                mTransakcija.Commit();
+                uspeh = true;
+            }
+            catch
+            {
+                mTransakcija.Rollback();
+                uspeh = false;
+            }
+            return uspeh;
+        }
+
+        // preklapajuca metoda kada dobijemo vise upita da se izvrsi u transakciji
+        public bool IzvrsiAzuriranje(List<string> ListaUpita)
+        // izvrzava azuriranje unos/brisanje/izmena 
+        // moze se dodeliti kao parametar lista od vise upita
+        // sada transakcija ima smisla, jer izvrsava vise upita u paketu
+        {
+
+            //
+            bool uspeh = false;
+            SqlConnection mKonekcija;
+            SqlCommand Komanda;
+            SqlTransaction mTransakcija = null;
+            try
+            {
+                mKonekcija = pKonekcija.DajKonekciju();
+                // aktivan kod  
+
+                // povezivanje
+                Komanda = new SqlCommand();
+                Komanda.Connection = mKonekcija;
+                Komanda = mKonekcija.CreateCommand();
+                // pokretanje
+                // NE TREBA OPEN JER DOBIJAMO OTVORENU KONEKCIJU KROZ KONSTRUKTOR
+                // mKonekcija.Open();
+                string Upit = "";
+                mTransakcija = mKonekcija.BeginTransaction();
+                Komanda.Transaction = mTransakcija;
+                for (int i = 0; i < ListaUpita.Count(); i++)
+                {
+                    Upit = ListaUpita[i];
+                    Komanda.CommandText = Upit;
+                    Komanda.ExecuteNonQuery();
+                }
+                mTransakcija.Commit();
+                uspeh = true;
+            }
+            catch
+            {
+                mTransakcija.Rollback();
+                uspeh = false;
+            }
+            return uspeh;
+        }
+
+
+
+        #endregion
+
+    }
+}

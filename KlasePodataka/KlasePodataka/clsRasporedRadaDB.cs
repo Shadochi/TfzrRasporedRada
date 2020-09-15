ad9fd41577e0072b097ee63576cd6b433906ddd4
Nikola Bobinac <nikolabobinac@gmail.com>
2020-09-15 02:22:21 +0200

    Added clsRasporedRadaDB.cs to communicate with DB

---
+++ KlasePodataka/KlasePodataka/clsRasporedRadaDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlDBUtils;
+namespace KlasePodataka
+{
+    public class clsRasporedRadaDB
+    {
+        //atributi
+        private clsSqlKonekcija objSqlKonekcija;
+        private clsSqlTabela objSqlTabela;
+        private DataSet dsRasporedRada;
+
+        //konstruktor
+        public clsRasporedRadaDB(string MSSQLInstanca, string NazivBazePodataka)
+        {
+            objSqlKonekcija = new clsSqlKonekcija(MSSQLInstanca, "", NazivBazePodataka);
+            objSqlKonekcija.OtvoriKonekciju();
+            objSqlTabela = new clsSqlTabela(objSqlKonekcija, "RasporedRada");
+            dsRasporedRada = new DataSet();
+        }
+
+        public DataSet DajSveIzRasporeda()
+        {
+            dsRasporedRada = objSqlTabela.DajPodatke("SELECT * FROM RasporedRada");
+            return dsRasporedRada;
+        }
+
+        public DataSet DajRasporedPremaKorisniku(string ImeIPrezime)
+        {
+            //Razdvojiti ime i prezime u niz stringova na osnovu razmaka
+            string[] imeIPrezime = ImeIPrezime.Split(' ');
+            dsRasporedRada = objSqlTabela.DajPodatke($"SELECT * FROM RasporedRada WHERE Ime = '{imeIPrezime[0]}' AND Prezime = '{imeIPrezime[1]}'");
+            return dsRasporedRada;
+        }
+
+        public void SnimiNoviUnosRada(clsRasporedRada objRasporedRada, out bool uspeh, out string tekstGreske)
+        {
+            string strDatum = $"{objRasporedRada.Datum.Month}/{objRasporedRada.Datum.Day}/{objRasporedRada.Datum.Year}";
+
+            string sqlInsertUpit = $"INSERT INTO RasporedRada VALUES('{objRasporedRada.Ime}','{objRasporedRada.Prezime}','{objRasporedRada.Klijent}', '{objRasporedRada.Sati}', '{strDatum}', '{objRasporedRada.Posao}')";
+            
+            try
+            {
+                uspeh = objSqlTabela.IzvrsiAzuriranje(sqlInsertUpit);
+                tekstGreske = string.Empty;
+            }
+            catch (Exception greska)
+            {
+                uspeh = false;
+                tekstGreske = greska.Message;
+            }
+        }
+    }
+}
